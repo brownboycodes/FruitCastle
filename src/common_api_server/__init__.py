515ceffebd48d89b2adedfc8434147d0d081ac8e
brownboycodes <ideatorrraj@gmail.com>
2021-10-22 00:06:13 +0530

    perf(APIs):recycle: :zap: : added response for routes:sparkles: :hammer: :construction:

--- src/common_api_server/__init__.py
+++ src/common_api_server/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,3 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+@app.route("/paypal-concept-data")
+def paypal_concept_data():
+    response_for_route = {
+        "error": "please mention the correct version of the paypal-concept-data API"}
+    return jsonify(response_for_route)
+
+
+@app.route("/paypal-concept-data/v1")
+def paypal_concept_data_v1():
+    response_for_route = {
+        "error": "please mention exactly what you want from version 1 of the paypal-concept-data API"}
+    return jsonify(response_for_route)
