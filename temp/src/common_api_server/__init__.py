8588c4405f24ef7206dc4bfd5ff44d7333c08754
brownboycodes <ideatorrraj@gmail.com>
2021-10-22 11:49:30 +0530

    :mute: chore(delete directory): deleted directory :wastebasket:

--- temp/src/common_api_server/__init__.py
+++
@@ -1,9 +0,0 @@
-import json
-
-json_file=open("src/data/users.json","r")
-json_file_raw=json_file.read()
-json_file_parsed=json.loads(json_file_raw)
-
-@app.route("/paypal-concept-data/v1/all-data")
-def paypal_concept_data_v1_all_data():
-    return jsonify(json_file_parsed)
