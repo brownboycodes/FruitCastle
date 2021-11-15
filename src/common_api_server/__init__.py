7412380514c30f0ae34aa4d7e590a410006f67fb
brownboycodes <ideatorrraj@gmail.com>
2021-11-15 11:41:30 +0530

    :label::zap: perf(routes): added more error handlers :goal_net::necktie:

--- src/common_api_server/__init__.py
+++ src/common_api_server/__init__.py
@@ -22,12 +22,12 @@ def hello():
     # return render_template("index.html")
 
 
-@app.route("/paypal-concept-data")
-def paypal_concept_data():
-    response_for_route = {
-        "error": "please mention the correct version of the paypal-concept-data API"}
-    return jsonify(response_for_route)
-
+# @app.route("/paypal-concept-data")
+# def paypal_concept_data():
+    # response_for_route = {
+    #     "error": "please mention the correct version of the paypal-concept-data API"}
+    # return jsonify(response_for_route)
+    # return render_template("dashboard.html",py_sent_data="yaml")
 
 @app.route("/paypal-concept-data/v1")
 def paypal_concept_data_v1():
@@ -45,17 +45,30 @@ json_file_parsed = json.loads(json_file_raw)
 def paypal_concept_data_v1_all_data():
     return jsonify(json_file_parsed)
 
-
+# @app.route('/paypal-concept-data/v1/docs')
+# def paypal_concept_data_v1_docs():
+    # return send_from_directory("paypal_concept_data/v1", "README.md")
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # Page not found.
-    # return send_from_directory(app.template_folder,"error_page.html")
     return make_response(
         render_template("error_page.html",error_code="404"),
         404
     )
 
+@app.errorhandler(400)
+def page_not_found(e):
+    return make_response(
+        render_template("error_page.html",error_code="400"),
+        400
+    )
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return make_response(
+        render_template("error_page.html",error_code="500"),
+        500
+    )    
 
 
 """
