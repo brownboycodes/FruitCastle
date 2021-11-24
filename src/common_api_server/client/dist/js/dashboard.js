adb080265d63142b489676d6c48845338012a040
brownboycodes <ideatorrraj@gmail.com>
2021-11-24 14:37:56 +0530

    :lipstick: style(DOCs): updated UI of code blocks :art:

--- src/common_api_server/client/dist/js/dashboard.js
+++ src/common_api_server/client/dist/js/dashboard.js
@@ -10,18 +10,58 @@ fetch(`${window.location.href}/v1/docs`)
     // console.log(sampleHtml);
     node.innerHTML = sampleHtml;
     hljs.highlightAll();
+    beautifyCode();
   });
 // console.log(window.location.href);
-  // console.log(current_data);
+// console.log(current_data);
 
 /* globals Chart:false, feather:false */
+function beautifyCode() {
+  //! needs work
+  let allCodeBlocks = Array.from(document.querySelectorAll("pre"));
+  console.log(allCodeBlocks);
 
+  for (const block of allCodeBlocks) {
+    let nodeAbove=block.nextSibling;
+    console.log(nodeAbove);
+    let wrapperDiv = document.createElement("div");
+    // wrapperDiv.style.margin = "10px";
+    // wrapperDiv.style.visibility = "visible";
+    wrapperDiv.classList.add("code-wrapper");
+    let fakeMenu = document.createElement("div");
+    fakeMenu.classList.add("fakeMenu");
+    fakeMenu.style.visibility = "visible";
+    let fakeClose = document.createElement("div");
+    fakeClose.classList.add("fakeButtons", "fakeClose");
+    fakeClose.style.visibility = "visible";
+    let fakeMinimize = document.createElement("div");
+    fakeMinimize.classList.add("fakeButtons", "fakeMinimize");
+    fakeMinimize.style.visibility = "visible";
+    let fakeZoom = document.createElement("div");
+    fakeZoom.classList.add("fakeButtons", "fakeZoom");
+    fakeZoom.style.visibility = "visible";
+    let fakeScreen = document.createElement("div");
+    fakeScreen.classList.add("fakeScreen");
+    fakeScreen.style.visibility = "visible";
+    fakeMenu.appendChild(fakeClose);
+    fakeMenu.appendChild(fakeMinimize);
+    fakeMenu.appendChild(fakeZoom);
+    wrapperDiv.appendChild(fakeMenu);
+    fakeScreen.appendChild(block);
+    // document.body.appendChild(fakeScreen);
+    wrapperDiv.appendChild(fakeScreen);
+    // nodeAbove.appendChild(wrapperDiv);
+    node.insertBefore(wrapperDiv,nodeAbove);
+    // console.log(block);
+  }
+  //!  needs work
+}
 
 (function () {
-  'use strict'
+  "use strict";
 
-  feather.replace({ 'aria-hidden': 'true' })
-/*
+  feather.replace({ "aria-hidden": "true" });
+  /*
   // Graphs
   var ctx = document.getElementById('myChart')
   // eslint-disable-next-line no-unused-vars
@@ -68,6 +108,4 @@ fetch(`${window.location.href}/v1/docs`)
     }
   })
   */
-})()
-
-
+})();
