0e02c2231e59c5f063a74cefd92ac37c63a3b43d
brownboycodes <ideatorrraj@gmail.com>
2022-04-06 13:50:53 +0530

    :bug: fix(compression): debugging :package::clamp:

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,5 @@
 from src.fruit_castle.main import app, socketio
 from src.fruit_castle.playpal.v3.v3_socket_events import *
-from flask_compress import Compress
-
-compress=Compress()
 
 if __name__ == "__main__":
-    compress.init_app(app)
     socketio.run(app)
